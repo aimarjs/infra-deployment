4ba9a4dccb8182eaf73bbf18556518de2890cb7b
Aimar Aareleid <aimar@snapit.ee>
2018-01-31 10:56:14 +0100

    update

--- post-install-frontend.sh
+++ post-install-frontend.sh
@@ -1,21 +1,6 @@
-AZURE_RESEARCH_GROUP='cosmospoc'
-AZURE_REGION='westeurope'
 NGINX_DOMAIN_NAME='cosmospoc.ml'
 NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
 NGINX_ENABLED_VHOSTS='/etc/nginx/sites-enabled'
-
-#### AZURE CREATE VM ####
-# az login 
-# az group create --location $AZURE_REGION --name $AZURE_RESEARCH_GROUP
-# az network public-ip create --name frontend-ip --resource-group $AZURE_RESEARCH_GROUP --location $AZURE_REGION
-# az network vnet create --name $AZURE_RESEARCH_GROUP-vnet --resource-group $AZURE_RESEARCH_GROUP --address-prefixes 10.10.0.0/16 --location $AZURE_REGION --subnet-name vmsubnet --subnet-prefix 10.10.1.0/24
-# az network nsg create --name $AZURE_RESEARCH_GROUP-nsg --resource-group $AZURE_RESEARCH_GROUP --location $AZURE_REGION
-# az network vnet subnet update --name vmsubnet --resource-group $AZURE_RESEARCH_GROUP --vnet-name $AZURE_RESEARCH_GROUP-vnet --network-security-group vmsubnet
-# az network nic create --resource-group $AZURE_RESEARCH_GROUP --name $AZURE_RESEARCH_GROUP-nic --subnet vmsubnet --vnet-name $AZURE_RESEARCH_GROUP-vnet --location $AZURE_REGION --public-ip-address frontend-ip
-# az vm create --name frontend --resource-group $AZURE_RESEARCH_GROUP --admin-username devops --admin-password !1qwerty123456 --image Ubuntu --authentication-type password --nics $AZURE_RESEARCH_GROUP-nic  --size Standard_DS1 --location $AZURE_REGION
-# az vm open-port --name frontend --port 22 --resource-group $AZURE_RESEARCH_GROUP --apply-to-subnet --priority 1000
-#OR
-# az network nsg rule create --name SSH --nsg-name vmsubnet --priority 1100 --resource-group clivm --source-address-prefix * --source-port-range * --destination-address-prefix * --destination-port-range 22 --protocol Tcp
 
 sudo apt-get update -y
 sudo apt-get install nginx -y
